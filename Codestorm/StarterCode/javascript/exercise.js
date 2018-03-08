f817ad1bdc81c147274fa17e76417f16c9cf8336
devon-the-editor <devonnaidoo13@gmail.com>
2018-03-08 13:41:10 +0200

    Update exercise.js

--- Codestorm/StarterCode/javascript/exercise.js
+++ Codestorm/StarterCode/javascript/exercise.js
@@ -13,18 +13,16 @@
 //Start writing the function here
 
 
-
-//This function confirms that if I select and item the right value is returned
 //Testing is an important part of coding and we want to test and confirm that if the user
-//selects a day in dropdown that the right value is returned
+//selects a day in the dropdown that the right value is returned.
 function connectDaysToIcons(){
     if(getDropdownValue() === "monday"){
         return 'You chose ' + getDropdownValue();
     } else if(getDropdownValue() === "tuesday"){
         return 'You chose ' + getDropdownValue();
     } 
-//Activity 3 - Complete the function by adding in the rest of days of the week
-//You can use the above else if as an example
+//Activity 3 - Complete the function by adding in the rest of days of the week.
+//You can use the above else if statements as an example.
 
 }
 
@@ -36,21 +34,21 @@ function connectDaysToIcons(){
 //'box_one'
 //'box_two'
 //'box_three'
-//Create the Array below
+//Create the Array below.
 
 
 //This function creates a checkbox as well as a label and line break
 //This function will work as a template for creating the above mentioned elements which means it can be called multiple times
 //Activity 6 - Add dayGoal as a parameter
 //dayGoal will take the Arrays that you've created in Activity
-function createCheckBox(dayGoal){
-//Activity 7.1 - Create a variable called createCheckbox and create an INPUT element using the createElement() method.
+function createCheckBox(){
+//Activity 7.1 - Create a variable called createCheckBox and create an INPUT element using the createElement() method.
 
 
-//Activity 7.2 - Create a variable called createLabel and create an LABEL element using the createElement() method.
+//Activity 7.2 - Create a variable called createLabel and create a LABEL element.
 
 
-//Activity 7.3 - Create a variable called createBreak and create an br(break) element using the createElement() method.
+//Activity 7.3 - Create a variable called createBreak and create a br(break) element.
 
 
 //Activity 8 - Create a variable called addTextToBox and create a text node using the createTextNode() method.
@@ -58,19 +56,19 @@ function createCheckBox(dayGoal){
 
 
 //Activity 9 - Use the setAttribute() to create a checkbox. Use your resources to learn how setAttribute works.
-//Copy and paste createCheckbox.setAttribute(); and add the appropriate parameters to create the checkbox 
+//Copy and paste createCheckBox.setAttribute(); and add the appropriate parameters to create the checkbox.
 
 
-//Activity 10 - Uncomment the code below to 'active' it
- 
+//Activity 10 - Uncomment the code below to 'active' it.
+    //createCheckBox.id = checkboxId[i];
 
-//Activity 11.1 - Append createCheckbox to createLabel
-
-
-//Activity 11.2 - Append addTextToBox to createLabel
+//Activity 11.1 - Append createCheckBox to createLabel.
 
 
-//Activity 11.3 - Append createBreak to createLabel
+//Activity 11.2 - Append addTextToBox to createLabel.
+
+
+//Activity 11.3 - Append createBreak to createLabel.
 
 
 //Activity 12.1 - Create a variable called element and set it equal to document.getElementById("goals");
@@ -91,14 +89,14 @@ function goalsForTheDay(selectedDay){
         } else if(selectedDay === 'tuesday'){
             createCheckBox(tuesdayGoals[i]);
         }
-//Activity 13.1 - Complete the function by adding in the rest of days of the week
-//You can use the above else if as an example
-   
+//Activity 13.1 - Complete the function by adding in the rest of days of the week.
+//You can use the above else if statement as an example.
+ 
 
         }
     }
 
-//Activity 13.2 - Call the goalsForTheDay and add in the appropriate parameter.
+//Activity 13.2 - Call the goalsForTheDay in the display function and add in the appropriate parameter.
 
 
 //The function below checks if all the checkboxes are checked for a particular day.
@@ -107,52 +105,64 @@ function goalsForTheDay(selectedDay){
     function checkboxTrue(selectedDay){
 //Activity 14 - This loop is broken. Find the problem and fix it.
     for(i = 0; i < checkboxId.length; i++){
-//These variables gets the element with the specified ID in this case it will get the checkboxes
+//These variables gets the element with the specified ID in this case it will get the checkboxes.
         var checkbox1 = document.getElementById(checkboxId[0]);
         var checkbox2 = document.getElementById(checkboxId[1]);
         var checkbox3 = document.getElementById(checkboxId[2]);
 
 //Activity 15.1 - Create a variable called monday and get the element with the ID 'monIcon'
 
+
 //Activity 15.2 - Create a variable called tuesday and get the element with the ID 'tuesIcon'
+
 
 //Activity 15.3 - Create a variable called wednesday and get the element with the ID 'wedIcon'
 
+
 //Activity 15.4 - Create a variable called thursday and get the element with the ID 'thursIcon'
+
 
 //Activity 15.5 - Create a variable called friday and get the element with the ID 'friIcon'
 
+
 //Activity 15.6 - Create a variable called saturday and get the element with the ID 'satIcon'
+
 
 //Activity 15.7 - Create a variable called sunday and get the element with the ID 'sunIcon'
 
 
+
 //These if states checks if all the each checkbox is checked for each day
             if(checkbox1.checked === true && checkbox2.checked === true && checkbox3.checked === true && selectedDay === 'monday'){
-                console.log('Works!' + checkboxId[i] + selectedDay);
-//If all the checkboxes are checked then the background color of the box for that day in the HTML DOM will change to green
-                monday.setAttribute("style", "background-color: #2EBD1B; color: white; border: 0px solid transparent;");
-            } else if(selectedDay === 'monday'){
-//If all the checkboxes are not checked then the background color of the box for that day in the HTML DOM will change to red
-                monday.setAttribute("style", "background-color: #BD1B1B; color: white; border: 0px solid transparent;");
-            }
+//Activity 16.1 - If all the checkboxes are checked then the background color of the box for that day in the HTML DOM should change to green
+//Use setAttribute() to change the background colour to green. Additionally change the text color to white and eliminate the border.
 
-            if(checkbox1.checked === true && checkbox2.checked === true && checkbox3.checked === true && selectedDay === 'tuesday'){
-                console.log('Works!' + checkboxId[i] + selectedDay);
-                tuesday.setAttribute("style", "background-color: #2EBD1B; color: white; border: 0px solid transparent;");
-            } else if(selectedDay === 'tuesday'){
-                tuesday.setAttribute("style", "background-color: #BD1B1B; color: white; border: 0px solid transparent;");
+            } else if(selectedDay === 'monday'){
+//Activity 16.2 - If all the checkboxes are not checked then the background color of the box for that day in the HTML DOM should change to red
+//Use setAttribute() to change the background colour to red. Additionally change the text color to white and eliminate the border.
+
             }
-//Activity 16 - Complete the function by adding in the rest of days of the week
-//You can use the above else if as an example
+//Activity 16.3 - Complete the function by adding in the rest of days of the week.
 
         }
     }
 
-//Activity 17 - Create a function called disableButton that disables the 'GO' button in the HTML DOM after the user clicks it
+//Activity 17.1 - Create a function called disableButton that disables the 'GO' button in the HTML DOM after the user clicks it
 //Call the function in the onclick attribute in the exercise.html file
+//Start your code here.
 
+
+//Activity 17.2 - Call the disableButton function in the onclick attribute of the GO button in the exercise.html 
 
 function display(){
 
+    
 }
+
+
+//Activity 18 - If you've made it to this activity that means your JavaScript is working perfectly.
+//For this activity you are going to use your creativity to style the webpage using CSS.
+//You have full creative freedom to style it however you fancy.
+//Start by creating a new css file and title it "your name".css and link it to the exercise.html file.
+//Remember to use your resources.
+//Have fun! :)
