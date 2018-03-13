bcf4bf0251d152c40a0745d5bb1215aa1148b83f
Sasha <sashaharrison1997@gmail.com>
2018-03-13 14:13:15 +0200

    Editing starter code and creating zip

--- Codestorm/CompletedCode/exercise/exercise.js
+++ Codestorm/CompletedCode/exercise/exercise.js
@@ -1,7 +1,9 @@
+
+/********************** Part 1**********************/
 //Activity 1 - Create 7 Arrays, 1 for each day of the week
 //Name each Array as follows:
 //Array 1 - mondaysGoals; Array 2: tuesdayGoals; Array 3: wednesdayGoals...etc
-//Each Array should contain only 3 properties that will represent the goals for that day
+//Each Array should contain only 3 elements that will represent the goals for that day
 //You have the freedom to choose any goals you wish.
 //Start coding here
 var mondayGoals = ['2KM Jog', ' 50 Squats', ' 50 Pushups'];
@@ -12,8 +14,16 @@ var fridayGoals = ['3KM Jog', ' 60 Squats', ' 10 Pushups'];
 var saturdayGoals = ['1KM Jog', ' 30 Squats', ' 50 Pushups'];
 var sundayGoals = ['5KM Jog', ' 10 Squats', ' 10 Pushups'];
 
-//Activity 2 - This function will get the value of the drop value and returns it.
-//Start by creating a function called getDropdownValue. 
+//Activity 2 - Create an Array called checkboxId
+//The must contain the following 3 properties:
+//'box_one'
+//'box_two'
+//'box_three'
+//Create the Array below.
+var checkboxId = ['box_one', 'box_two', 'box_three'];
+
+//Activity 3 - This function will get the value of the drop value and returns it.
+//Start by creating a function called getDropdownValue.
 //Inside create a variable called days and set it equal to document.getElementById('days').value;
 //Finally return the value by using the return method.
 //Start writing the function here
@@ -22,16 +32,17 @@ function getDropdownValue(){
     return days;
 }
 
-//Testing is an important part of coding and we want to test and confirm that if the user
-//selects a day in the dropdown that the right value is returned.
+//Activity 4 - Testing is an important part of coding and we want to test and confirm that if the user
+//selects a day in the dropdown that the right value is returned or that a value is returned at all.
+//Write a function called testDays.  Inside the function, call getDropdownValue
+//and save its return value to a variable.  Then print "You chose " and the value
+//of your variable to the console.
 function connectDaysToIcons(){
     if(getDropdownValue() === "monday"){
         return 'You chose ' + getDropdownValue();
     } else if(getDropdownValue() === "tuesday"){
         return 'You chose ' + getDropdownValue();
-    } 
-//Activity 3 - Complete the function by adding in the rest of days of the week.
-//You can use the above else if statements as an example.
+    }
     else if(getDropdownValue() === "wednesday"){
         return 'You chose ' + getDropdownValue();
     } else if(getDropdownValue() === "thursday"){
@@ -45,8 +56,8 @@ function connectDaysToIcons(){
     }
 }
 
-//Activity 4 - At the bottom of this page is a function called display.
-//Inside the display function add this line of code - console.log(connectDaysToIcons()); 
+//Activity 5 - At the bottom of this page is a function called display.
+//Inside the display function add this line of code - console.log(connectDaysToIcons());
 
 //Activity 5 - Create an Array called checkboxId
 //The must contain the following 3 properties:
@@ -56,19 +67,21 @@ function connectDaysToIcons(){
 //Create the Array below.
 var checkboxId = ['box_one', 'box_two', 'box_three'];
 
+/********************** Part 2 **********************/
+
 //This function creates a checkbox as well as a label and line break
 //This function will work as a template for creating the above mentioned elements which means it can be called multiple times
 //Activity 6 - Add dayGoal as a parameter
 //dayGoal will take the Arrays that you've created in Activity
 function createCheckBox(dayGoal){
 //Activity 7.1 - Create a variable called createCheckBox and create an INPUT element using the createElement() method.
-    var createCheckBox = document.createElement("INPUT");
+    var newCheckBox = document.createElement("INPUT");
 
 //Activity 7.2 - Create a variable called createLabel and create a LABEL element.
-    var createLabel = document.createElement("LABEL");
+    var newLabel = document.createElement("LABEL");
 
 //Activity 7.3 - Create a variable called createBreak and create a br(break) element.
-    var createBreak = document.createElement("br");
+    var newBreak = document.createElement("br");
 
 //Activity 8 - Create a variable called addTextToBox and create a text node using the createTextNode() method.
 //Use dayGoal as a parameter.
@@ -76,25 +89,27 @@ function createCheckBox(dayGoal){
 
 //Activity 9 - Use the setAttribute() to create a checkbox. Use your resources to learn how setAttribute works.
 //Copy and paste createCheckBox.setAttribute(); and add the appropriate parameters to create the checkbox.
-    createCheckBox.setAttribute("type", "checkbox");
+    newCheckBox.setAttribute("type", "checkbox");
 
 //Activity 10 - Uncomment the code below to 'active' it.
-    createCheckBox.id = checkboxId[i];
+    newCheckBox.id = checkboxId[i];
 
 //Activity 11.1 - Append createCheckBox to createLabel.
-    createLabel.appendChild(createCheckBox);
+  newLabel.appendChild(newCheckBox);
 
 //Activity 11.2 - Append addTextToBox to createLabel.
-    createLabel.appendChild(addTextToBox);
+    newLabel.appendChild(addTextToBox);
 
 //Activity 11.3 - Append createBreak to createLabel.
-    createLabel.appendChild(createBreak);
+    newLabel.appendChild(newBreak);
 
 //Activity 12.1 - Create a variable called element and set it equal to document.getElementById("goals");
     var element = document.getElementById("goals");
 //Activity 12.2 - Append createLabel element
-    element.appendChild(createLabel);
+    element.appendChild(newLabel);
 }
+
+/********************** Part 3 **********************/
 
 //This function creates a checkbox for each day based on the user's selection
 //It uses the function above to create the checkbox
@@ -120,7 +135,7 @@ function goalsForTheDay(selectedDay){
             createCheckBox(saturdayGoals[i]);
         } else if(selectedDay === 'sunday'){
             createCheckBox(sundayGoals[i]);
-        }   
+        }
 
         }
     }
@@ -128,12 +143,14 @@ function goalsForTheDay(selectedDay){
 //Activity 13.2 - Call the goalsForTheDay in the display function and add in the appropriate parameter.
 
 
+
+/********************** Part 4 **********************/
 //The function below checks if all the checkboxes are checked for a particular day.
 //If all of the checkboxes are checked then the box for that specific day will turn green.
 //If any of the checkboxes are not checked then the box for that specific day will turn red.
     function checkboxTrue(selectedDay){
 //Activity 14 - This loop is broken. Find the problem and fix it.
-    for(i = 0; i < checkboxId.length; i++){
+    //for(i = 0; i < checkboxId.length; i++){
 //These variables gets the element with the specified ID in this case it will get the checkboxes
         var checkbox1 = document.getElementById(checkboxId[0]);
         var checkbox2 = document.getElementById(checkboxId[1]);
@@ -155,51 +172,51 @@ function goalsForTheDay(selectedDay){
         var sunday = document.getElementById('sunIcon');
 
 //These if states checks if all the each checkbox is checked for each day
-            if(checkbox1.checked === true && checkbox2.checked === true && checkbox3.checked === true && selectedDay === 'monday'){
-//Activity 16.1 - If all the checkboxes are checked then the background color of the box for that day in the HTML DOM should change to green
-//Use setAttribute() to change the background colour to green. Additionally change the text color to white and eliminate the border.
+            if(checkbox1.checked === true && checkbox2.checked === true && checkbox3.checked === true){
+              //Activity 16.1 - If all the checkboxes are checked then the background color of the box for that day in the HTML DOM should change to green
+              //Use setAttribute() to change the background colour to green. Additionally change the text color to white and eliminate the border.
+              if(selectedDay === 'monday'){
                 monday.setAttribute("style", "background-color: #2EBD1B; color: white; border: 0px solid transparent;");
-            } else if(selectedDay === 'monday'){
-//Activity 16.2 - If all the checkboxes are not checked then the background color of the box for that day in the HTML DOM should change to red
-//Use setAttribute() to change the background colour to red. Additionally change the text color to white and eliminate the border.
-                monday.setAttribute("style", "background-color: #BD1B1B; color: white; border: 0px solid transparent;");
-            }
-//Activity 16.3 - Complete the function by adding in the rest of days of the week.
-//You can use the above else if statement as an example.
-            if(checkbox1.checked === true && checkbox2.checked === true && checkbox3.checked === true && selectedDay === 'tuesday'){
+              }
+              //Activity 16.2 - The if statement above changes the background of the monday box to green.
+              //Below, use if statements to do the same for every day of the week (there should be 7 total).
+              //fill in the rest of the if statements here...
+              else if (selectedDay === 'tuesday'){
                 tuesday.setAttribute("style", "background-color: #2EBD1B; color: white; border: 0px solid transparent;");
-            } else if(selectedDay === 'tuesday'){
-                tuesday.setAttribute("style", "background-color: #BD1B1B; color: white; border: 0px solid transparent;");
-            }
-            if(checkbox1.checked === true && checkbox2.checked === true && checkbox3.checked === true && selectedDay === 'wednesday'){
+              } else if (selectedDay === 'wednesday'){
                 wednesday.setAttribute("style", "background-color: #2EBD1B; color: white; border: 0px solid transparent;");
-            } else if(selectedDay === 'wednesday'){
-                wednesday.setAttribute("style", "background-color: #BD1B1B; color: white; border: 0px solid transparent;");
-            }
-            if(checkbox1.checked === true && checkbox2.checked === true && checkbox3.checked === true && selectedDay === 'thursday'){
+              } else if (selectedDay === 'thursday'){
                 thursday.setAttribute("style", "background-color: #2EBD1B; color: white; border: 0px solid transparent;");
-            } else if(selectedDay === 'thursday'){
-                thursday.setAttribute("style", "background-color: #BD1B1B; color: white; border: 0px solid transparent;");
-            }
-
-            if(checkbox1.checked === true && checkbox2.checked === true && checkbox3.checked === true && selectedDay === 'friday'){
+              } else if (selectedDay === 'friday'){
                 friday.setAttribute("style", "background-color: #2EBD1B; color: white; border: 0px solid transparent;");
-            } else if(selectedDay === 'friday'){
-                friday.setAttribute("style", "background-color: #BD1B1B; color: white; border: 0px solid transparent;");
-            }
-
-            if(checkbox1.checked === true && checkbox2.checked === true && checkbox3.checked === true && selectedDay === 'saturday'){
+              } else if (selectedDay === 'saturday'){
                 saturday.setAttribute("style", "background-color: #2EBD1B; color: white; border: 0px solid transparent;");
-            } else if(selectedDay === 'saturday'){
+              } else{
+                sunday.setAttribute("style", "background-color: #2EBD1B; color: white; border: 0px solid transparent;");
+              }
+            }else{
+              //Activity 16.3 - If even one checkbox is not ticked, then the background color of the box
+              //for that day in the HTML DOM should change to red.
+              //Use conditionals for each day of the week to change the background
+              //colour of the appropriate box to red (as above).
+              //Additionally, change the text color to white and eliminate the border.
+              if(selectedDay === 'monday'){
+                monday.setAttribute("style", "background-color: #BD1B1B; color: white; border: 0px solid transparent;");
+              } else if (selectedDay === 'tuesday'){
+                tuesday.setAttribute("style", "background-color: #BD1B1B; color: white; border: 0px solid transparent;");
+              } else if (selectedDay === 'wednesday'){
+                wednesday.setAttribute("style", "background-color: #BD1B1B; color: white; border: 0px solid transparent;");
+              } else if (selectedDay === 'thursday'){
+                thursday.setAttribute("style", "background-color: #BD1B1B; color: white; border: 0px solid transparent;");
+              } else if (selectedDay === 'friday'){
+                friday.setAttribute("style", "background-color: #BD1B1B; color: white; border: 0px solid transparent;");
+              } else if (selectedDay === 'saturday'){
                 saturday.setAttribute("style", "background-color: #BD1B1B; color: white; border: 0px solid transparent;");
+              } else{
+                sunday.setAttribute("style", "background-color: #BD1B1B; color: white; border: 0px solid transparent;");
+              }
             }
 
-            if(checkbox1.checked === true && checkbox2.checked === true && checkbox3.checked === true && selectedDay === 'sunday'){
-                sunday.setAttribute("style", "background-color: #2EBD1B; color: white; border: 0px solid transparent;");
-            } else if(selectedDay === 'sunday'){
-                sunday.setAttribute("style", "background-color: #BD1B1B; color: white; border: 0px solid transparent;");
-            }
-        }
     }
 
 //Activity 17.1 - Create a function called disableButton that disables the 'GO' button in the HTML DOM after the user clicks it
@@ -214,7 +231,7 @@ function display(){
     console.log(connectDaysToIcons());
     goalsForTheDay(getDropdownValue());
 }
-//Activity 17.2 - Call the disableButton function in the onclick attribute of the GO button in the exercise.html 
+//Activity 17.2 - Call the disableButton function in the onclick attribute of the GO button in the exercise.html
 
 //Activity 18 - If you've made it to this activity that means your JavaScript is working perfectly.
 //For this activity you are going to use your creativity to style the webpage using CSS.
--- Codestorm/StarterCode/exercise/exercise.js
+++ Codestorm/StarterCode/exercise/exercise.js
@@ -1,26 +1,26 @@
 /* You have been asked to create an exercise app to keep track of your daily exercise goals.
 The app has to give an indication of whether the all the goals for a specific day has been met, for example,
-if all the goals for a specific day is met then the box in the web page that contains that day will turn green, 
+if all the goals for a specific day is met then the box in the web page that contains that day will turn green,
 but if any one of goals is not the box that contains that day will turn red.
 */
 
 /********************** Part 1**********************/
 //Activity 1 - Create 7 Arrays, 1 for each day of the week
 //Name each Array as follows:
-//Array 1 - mondaysGoals; Array 2: tuesdayGoals; Array 3: wednesdayGoals...etc
-//Each Array should contain only 3 properties that will represent the goals for that day.
+//Array 1 - mondayGoals; Array 2: tuesdayGoals; Array 3: wednesdayGoals...etc
+//Each Array should contain only 3 elements that will represent the goals for that day.
 //You have the freedom to choose any goals you wish.
 //Start coding here
 
 //Activity 2 - Create an Array called checkboxId
-//The must contain the following 3 properties:
+//It must contain the following 3 elements:
 //'box_one'
 //'box_two'
 //'box_three'
 //Create the Array below.
 
 //Activity 3 - Create a function that will get the value of the drop value and returns it.
-//Start by creating a function called getDropdownValue. 
+//Start by creating a function called getDropdownValue.
 //Inside create a variable called days and set it equal to document.getElementById('days').value;
 //Finally return the value by using the return method.
 //Start writing the function here
@@ -28,54 +28,57 @@ but if any one of goals is not the box that contains that day will turn red.
 
 //Activity 4 - Testing is an important part of coding and we want to test and confirm that if the user
 //selects a day in the dropdown that the right value is returned or that a value is returned at all.
-//Write a function called testDays that prints: "You chose " and the day the user has selected from the dropdown)
-//You can print it in the console.
+//Write a function called testDays.  Inside the function, call getDropdownValue
+//and save its return value to a variable.  Then print "You chose " and the value
+//of your variable to the console.
+//Start writing the function here
 
 
 //Activity 5 - At the bottom of this page is a function called display.
 //Inside the display function call the testDays function you've created.
 //The display() function is already called in the exercise.html file
 //The element looks like this <input type="submit" value="GO" id="button" onclick="display();">
-
+//When you've completed this activity, you should be able to see the day you select in the console.
 
 /********************** Part 2 **********************/
-//This function creates a checkbox as well as a label and line break
-//This function will work as a template for creating the above mentioned elements which means it can be called multiple times
-//Activity 6 - Add dayGoal as a parameter
-//dayGoal will take the Arrays that you've created in Activity
+//This function creates a checkbox, a label, and a line break
+//This function will work as a template for creating these DOM elements,
+//which means it can be called multiple times.
+//Activity 6 - Add dayGoal as a parameter for createCheckBox
+//The dayGoal parameter will take the Arrays that you've created in Activity 1
 function createCheckBox(){
-//Activity 7.1 - Create a variable called createCheckBox and create an INPUT element using the createElement() method.
+//Activity 7.1 - Create a variable called newCheckBox and create an INPUT element using the createElement() method.
 
 
-//Activity 7.2 - Create a variable called createLabel and create a LABEL element.
+//Activity 7.2 - Create a variable called newLabel and create a LABEL element.
 
 
-//Activity 7.3 - Create a variable called createBreak and create a br(break) element.
+//Activity 7.3 - Create a variable called newBreak and create a br(break) element.
 
 
 //Activity 8 - Create a variable called addTextToBox and create a text node using the createTextNode() method.
-//Use dayGoal as a parameter.
+//Pass dayGoal as the parameter to createTextNode.
 
 
 //Activity 9 - Use the setAttribute() to create a checkbox. Use your resources to learn how setAttribute works.
 //Copy and paste createCheckBox.setAttribute(); and add the appropriate parameters to create the checkbox.
 
 
-//Activity 10 - Uncomment the code below to 'active' it.
+//Activity 10 - Uncomment the code below to 'activate' it.
     //createCheckBox.id = checkboxId[i];
 
-//Activity 11.1 - Append createCheckBox to createLabel.
+//Activity 11.1 - Use appendChild to append newCheckBox to newLabel.
 
 
-//Activity 11.2 - Append addTextToBox to createLabel.
+//Activity 11.2 - Append addTextToBox to newLabel.
 
 
-//Activity 11.3 - Append createBreak to createLabel.
+//Activity 11.3 - Append newBreak to newLabel.
 
 
 //Activity 12.1 - Create a variable called element and set it equal to document.getElementById("goals");
 
-//Activity 12.2 - Append createLabel element
+//Activity 12.2 - Append newLabel element
 
 }
 
@@ -85,31 +88,32 @@ function createCheckBox(){
 //It uses the createCheckBox function to create the checkbox
 //The function contains a parameter called selectedDay which takes in the user selection
 //based on that day the specific goals for that day will appear in the HTML DOM
-//Activity 13.1 - Inside the for loop of the goalsForTheDay function create an if statement
-//that checks which day the user had selected in the dropdown and then based on that day
-//use the createCheckBox function to create a checkbox that contains the goals for that.
-//Example: if(selectedDay === 'monday') then create a box for that day {createCheckBox(mondayGoals[i]);}
 function goalsForTheDay(selectedDay){
 
-    for(i = 0; i < mondayGoals.length; i++){
-//Start coding here 
+  //Activity 13.1 - Inside the for loop of the goalsForTheDay function create an if statement
+  //that checks which day the user had selected in the dropdown and then based on that day
+  //use the createCheckBox function to create a checkbox that contains the goals for that.
+  //Example: if(selectedDay === 'monday') then create a box for that day {createCheckBox(mondayGoals[i]);}
+  for(i = 0; i < mondayGoals.length; i++){
+    //Start coding here
 
-        }
-    }
+  }
+}
 
-//Activity 13.2 - Call the goalsForTheDay in the display function and add in the appropriate parameter.
+//Activity 13.2 - In the display function at the bottom of this file, call the goalsForTheDay function,
+//passing the variable "selected as the parameter.  After you've finished this step,
+//you should see the check boxes appear on the left panel when you select a day and click "Go"
 
 
+/********************** Part 4 **********************/
 //The function below checks if all the checkboxes are checked for a particular day.
 //If all of the checkboxes are checked then the box for that specific day will turn green.
 //If any of the checkboxes are not checked then the box for that specific day will turn red.
-    function checkboxTrue(selectedDay){
-//Activity 14 - This loop is broken. Find the problem and fix it.
-    for(i = 0; i < checkboxId.length; i++){
+function checkboxTrue(selectedDay){
 //These variables gets the element with the specified ID in this case it will get the checkboxes.
-        var checkbox1 = document.getElementById(checkboxId[0]);
-        var checkbox2 = document.getElementById(checkboxId[1]);
-        var checkbox3 = document.getElementById(checkboxId[2]);
+  var checkbox1 = document.getElementById(checkboxId[0]);
+  var checkbox2 = document.getElementById(checkboxId[1]);
+  var checkbox3 = document.getElementById(checkboxId[2]);
 
 //Activity 15.1 - Create a variable called monday and get the element with the ID 'monIcon'
 
@@ -133,35 +137,45 @@ function goalsForTheDay(selectedDay){
 
 
 
-//These if states checks if all the each checkbox is checked for each day
-            if(checkbox1.checked === true && checkbox2.checked === true && checkbox3.checked === true && selectedDay === 'monday'){
-//Activity 16.1 - If all the checkboxes are ticked then the background color of the box for that day in the HTML DOM should change to green.
-//Use setAttribute() to change the background colour to green. Additionally change the text color to white and eliminate the border.
+//This if statements checks if all of the checkboxes are checked
+    if(checkbox1.checked === true && checkbox2.checked === true && checkbox3.checked === true){
+      //If all the checkboxes are ticked then the background color of the box
+      //for that day in the HTML DOM should change to green.
+      if(selectedDay === 'monday'){
+      //Activity 16.1 - Use setAttribute() to change the background colour of the monday variable to green.
+      //Additionally change the text color to white and eliminate the border.
+      }
+      //Activity 16.2 - The if statement above changes the background of the monday box to green.
+      //Below, use if statements to do the same for every day of the week (there should be 7 total).
+      //fill in the rest of the if statements here...
 
-            } else if(selectedDay === 'monday'){
-//Activity 16.2 - If even one checkbox is not ticked then the background color of the box for that day in the HTML DOM should change to red.
-//Use setAttribute() to change the background colour to red. Additionally change the text color to white and eliminate the border.
+    } else {
+      //Activity 16.3 - If even one checkbox is not ticked, then the background color of the box
+      //for that day in the HTML DOM should change to red.
+      //Use conditionals for each day of the week to change the background
+      //colour of the appropriate box to red (as above).
+      //Additionally, change the text color to white and eliminate the border.
 
-            }
-//Activity 16.3 - Complete the function by adding in the rest of days of the week.
-//You can use the above else if statement as an example.
-
-        }
     }
+
+}
 
 //Activity 17.1 (OPTIONAL) - Create a function called disableButton that disables the 'GO' button in the HTML DOM after the user clicks it
 //Call the function in the onclick attribute in the exercise.html file.
 //Start your code here.
 
 
-//Activity 17.2 - Call the disableButton function in the onclick attribute of the GO button in the exercise.html 
+//Activity 17.2 - Call the disableButton function in the onclick attribute of the GO button in the exercise.html
 
 function display(){
+  //complete Activity 5 here
 
-    
+  var selected = getDropdownValue();
+  //Activity 13.2 - Call the goalsForTheDay function with selected as a parameter.
+
 }
 
-
+/********************** Part 5 **********************/
 //Activity 18 - If you've made it to this activity that means your JavaScript is working perfectly.
 //For this activity you are going to use your creativity to style the webpage using CSS.
 //You have full creative freedom to style it however you fancy.
